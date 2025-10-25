31c3291d633f0c180582e1332c891481c9788f78
dHRvBiyAn1 <dhruvbiyani022@gmail.com>
2025-10-25 11:41:15 +0530

    Modified user entity

--- src/main/java/com/project/auth/service/AuthServiceImpl.java
+++ src/main/java/com/project/auth/service/AuthServiceImpl.java
@@ -11,7 +11,6 @@ import com.project.auth.exception.UsernameAlreadyExistsException;
 import com.project.auth.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
